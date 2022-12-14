2a5b197dc84f5e5e091f7b93f8f3dcec1bc090f0
twt898xu <xumengcheng123@foxmail.com>
2022-12-14 10:02:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# vue template explorer
+
+Playground for exploring how the Vue template compiler compiles your template.
+
+[Try it out](https://template-explorer.vuejs.org).
+
+<p align="center">
+  <img width="809px" src="https://raw.githubusercontent.com/vuejs/vue-template-explorer/master/media/screenshot.png">
+</p>
---
+++ src/App.vue
@@ -0,0 +1,201 @@
+<template>
+  <div id="app">
+    <h1>Vue Template Explorer (Vue version: {{version}})</h1>
+    <label class="with-toggle">
+      <input type="checkbox" v-model="stripWith">
+      Strip with?
+    </label>
+    <label class="server-toggle">
+      <input type="checkbox" v-model="serverRender">
+      Server Render?
+    </label>
+    <div class="main">
+      <codemirror
+        :value="input"
+        :options="inputOptions"
+        @input="onInput">
+      </codemirror>
+      <codemirror
+        :value="output.ast"
+        :options="astOptions">
+      </codemirror>
+      <codemirror
+        :value="output.code"
+        :options="outputOptions">
+      </codemirror>
+      <pre class="error" v-if="output.errors.length">
+        <div v-for="e in output.errors" :key="String(e)">{{e}}</div>
+      </pre>
+    </div>
+  </div>
+</template>
+
+<script>
+/* globals Vue, VueTemplateCompiler, renderVueComponentToString */
+
+// Vue and compilers used in the actual compilation are loaded from CDN so
+// they are always the latest versions.
+
+import 'codemirror/mode/vue/vue.js'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/base16-dark.css'
+import 'codemirror/theme/base16-light.css'
+import 'codemirror/theme/monokai.css'
+
+import 'codemirror/addon/fold/foldgutter.css'
+import 'codemirror/addon/fold/brace-fold.js'
+import 'codemirror/addon/fold/foldcode.js'
+import 'codemirror/addon/fold/foldgutter.js'
+import 'codemirror/addon/fold/indent-fold.js'
+
+import { codemirror } from 'vue-codemirror'
+import debounce from 'lodash.debounce'
+import beautify from 'js-beautify'
+import stripWith from 'vue-template-es2015-compiler'
+import safeJsonStringify from 'safe-json-stringify'
+
+export default {
+  name: 'app',
+  components: { codemirror },
+  data () {
+    return {
+      input: '',
+      version: Vue.version,
+      stripWith: false,
+      serverRender: false,
+      inputOptions: {
+        tabSize: 2,
+        mode: 'text/html',
+        theme: 'base16-light',
+        lineNumbers: true,
+        line: true,
+        // foldGutter: true,
+      },
+      outputOptions: {
+        tabSize: 2,
+        mode: 'text/javascript',
+        readOnly: true,
+        theme: 'base16-dark'
+      },
+      astOptions:{
+        tabSize: 2,
+        mode: {
+          name:'javascript',
+          json:true,
+        },
+        foldGutter: true,
+        gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+        readOnly: true,
+        theme: 'monokai',
+        lineNumbers: true,
+        line: true
+      }
+    }
+  },
+  computed: {
+    output () {
+      if (!this.input.trim()) {
+        return { errors: [], code: '' }
+      }
+      const res = VueTemplateCompiler.compile(this.input, { preserveWhitespace: false })
+      if (!res.errors.length) {
+        let code = this.serverRender
+          ? this.compileServer()
+          : `function render () {${res.render.toString()}}`
+        if (this.stripWith) {
+          code = stripWith(code)
+        }
+
+        return {
+          errors: [],
+          code: beautify(code, { indent_size: 2, wrap_line_length: 80, jslint_happy: true }),
+          ast: beautify(safeJsonStringify(res.ast), { indent_size: 2, wrap_line_length: 80, jslint_happy: true }),
+        }
+      } else {
+        return {
+          errors: res.errors,
+          code: '',
+          ast:''
+        }
+      }
+    }
+  },
+  mounted () {
+    const hashInput = window.location.hash.slice(1)
+    this.input = hashInput
+      ? decodeURIComponent(hashInput)
+      : `<div id="app">{{ msg }}</div>`
+    this.setHeight()
+    window.addEventListener('resize', this.setHeight)
+  },
+  methods: {
+    onInput: debounce(function (input) {
+      this.input = input
+      window.location.hash = encodeURIComponent(input)
+    }, 500),
+
+    setHeight () {
+      const mirrors = [...this.$el.querySelectorAll('.CodeMirror')]
+      if(!mirrors[0])return
+      const top = mirrors[0].getBoundingClientRect().top
+      const height = window.innerHeight - top
+      mirrors.forEach(m => m.style.height = height + 'px')
+    },
+    compileServer() {
+      Vue.config.silent = true
+      const vm = new Vue({template: this.input})
+      renderVueComponentToString(vm, () => {})
+      Vue.config.silent = false
+      return vm.$options.render.toString()
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Menlo, Consolas, monospace;
+  font-size: 14px;
+  color: #333;
+  margin: 0;
+}
+
+h1 {
+  padding: 0 10px;
+  font-size: 1.2em;
+}
+
+.main {
+  position: relative;
+  height: 100%;
+  display: flex;
+}
+
+.vue-codemirror {
+  line-height: 1.4em;
+  width: 50%;
+}
+
+.error {
+  position: absolute;
+  z-index: 999;
+  top: 0;
+  right: 0;
+  left: 50%;
+  background-color: #f33;
+  font-size: .9em;
+  color: #fff;
+  font-family: Menlo;
+  padding: 1em;
+  margin: 0;
+}
+
+.with-toggle, .server-toggle {
+  position: fixed;
+  top: 1em;
+  right: 15px;
+}
+.server-toggle {
+  right: 150px;
+}
+</style>
